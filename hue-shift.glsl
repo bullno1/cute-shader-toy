2d20d9f51a2f93d2b6ae73a48d477df83f450d24
Bach Le <bach@bullno1.com>
2026-01-13 19:31:08 +0800

    Add hue shift shader

---
+++ hue-shift.glsl
@@ -0,0 +1,12 @@
+#include "blend.shd"
+
+layout (set = 3, binding = 1) uniform shd_uniforms {
+	// @param name=hue type=float min=0.0 max=1.0 step=0.01
+    float hue;
+};
+
+vec4 shader(vec4 color, vec2 pos, vec2 screen_uv, vec4 params) {
+	vec3 hsv = rgb_to_hsv(color.rgb);
+	vec4 new_olor = vec4(hsv_to_rgb(vec3(hue, 0.0, 0.0) + hsv), color.a);
+	return new_olor;
+}
