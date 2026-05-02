9517519b4b51c43c524b393997ac1ba6f4f03b0d
Bach Le <bach@bullno1.com>
2026-05-02 10:08:20 +0700

    Clamp uv in glow shader

--- glow.glsl
+++ glow.glsl
@@ -1,11 +1,11 @@
 layout (set = 3, binding = 1) uniform shd_uniforms {
-	// @param name=glow_size type=float2 default.x=0.6 default.y=0.6
+	// @param name=glow_size type=float2 min=0.0 default.x=0.6 default.y=0.6 step=0.1
 	vec2 glow_size;
 };
 
-vec4 shader(vec4 color, vec2 pos, vec2 screen_uv, vec4 params) {
+vec4 shader(vec4 color, ShaderParams params) {
 	// @param name=glow_color type=color target=attribute default.r=1.0 default.g=0.85 default.b=0.20 default.a=1.0
-	vec4 glow_color = params;
+	vec4 glow_color = params.attributes;
 	vec4 original_color = texture(u_image, smooth_uv(v_uv, u_texture_size));
 	if (original_color.a > 0.0) { return original_color; }
 
@@ -19,7 +19,8 @@ vec4 shader(vec4 color, vec2 pos, vec2 screen_uv, vec4 params) {
 	float y_max = clamp(px_coord.y + glow_size.y, 0.0, u_texture_size.y);
 	for (float x = x_min; x <= x_max; x += 0.01) {
 		for (float y = y_min; y <= y_max; y += 0.01) {
-			vec4 neighbor_color = texture(u_image, smooth_uv(vec2(x, y) / u_texture_size, u_texture_size));
+			vec2 sampled_uv = clamp(vec2(x, y) / u_texture_size, params.uv_min, params.uv_max);
+			vec4 neighbor_color = texture(u_image, sampled_uv);
 			if (neighbor_color.a > 0.0) {
 				float x_diff = x - px_coord.x;
 				float y_diff = y - px_coord.y;
